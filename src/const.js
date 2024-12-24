3cb45dbb6988d177a144063afd38a3ef0a4b5a52
wit <witwitchayakarn@gmail.com>
2024-12-24 14:02:19 +0700

    Split to sdrive-hub-cleaner

--- src/const.js
+++ src/const.js
@@ -7,12 +7,3 @@ export const BRACE_STATIC_FILES = 'brace-001.appspot.com';
 
 export const EXTRACT = 'Extract';
 export const EXTRACT_LOG = 'ExtractLog';
-
-export const BACKUP_BUCKET = 'sdrive-hub-backup';
-
-export const FILE_INFO = 'FileInfo';
-export const BUCKET_INFO = 'BucketInfo';
-export const FILE_LOG = 'FileLog';
-export const FILE_WORK_LOG = 'FileWorkLog';
-
-export const DELETED = 'Deleted';
--- src/data.js
+++ src/data.js
@@ -1,13 +1,8 @@
-import { Datastore, PropertyFilter, and } from '@google-cloud/datastore';
-import { Storage } from '@google-cloud/storage';
+import { Datastore, PropertyFilter } from '@google-cloud/datastore';
 
-import {
-  VERIFY_LOG, NOTIFY_LOG, ACKNOWLEDGE_LOG, EXTRACT, EXTRACT_LOG, BACKUP_BUCKET,
-  FILE_INFO, FILE_LOG, FILE_WORK_LOG, DELETED,
-} from './const';
+import { VERIFY_LOG, NOTIFY_LOG, ACKNOWLEDGE_LOG, EXTRACT, EXTRACT_LOG } from './const';
 
 const datastore = new Datastore();
-const storage = new Storage();
 
 const queryData = async (query, readOnly) => {
   const transaction = datastore.transaction({ readOnly });
@@ -36,21 +31,6 @@ const deleteData = async (keys, nKeys = 64) => {
       await transaction.rollback();
       throw e;
     }
-  }
-};
-
-const deleteFiles = async (bucketName, paths, nItems = 32) => {
-  const bucket = storage.bucket(bucketName);
-  for (let i = 0; i < paths.length; i += nItems) {
-    const selectedPaths = paths.slice(i, i + nItems);
-    await Promise.all(
-      selectedPaths.map(path => {
-        const bucketFile = bucket.file(path);
-        return bucketFile.delete().catch(error => {
-          console.log(`In deleteFiles, ${path} has error:`, error);
-        });
-      })
-    );
   }
 };
 
@@ -250,129 +230,11 @@ const deleteExtractLogs = async (logs) => {
   await deleteData(keys, 256);
 };
 
-const getDeletedFileInfos = async () => {
-  const dt = Date.now() - (31 * 24 * 60 * 60 * 1000);
-  const date = new Date(dt);
-
-  const query = datastore.createQuery(FILE_INFO);
-  query.filter(and([
-    new PropertyFilter('status', '=', DELETED),
-    new PropertyFilter('updateDate', '<', date),
-  ])); // Need Composite Index Configuration in index.yaml in sdrive-hub
-  query.order('updateDate', { descending: false });
-  query.limit(2400);
-
-  const entities = await queryData(query, true);
-
-  const infos = [];
-  for (const entity of entities) {
-    const info = {
-      path: entity[datastore.KEY].name,
-      status: entity.status,
-      size: entity.size,
-      createDate: entity.createDate,
-      updateDate: entity.updateDate,
-    };
-    infos.push(info);
-  }
-  return infos;
-};
-
-/*const getObsoleteSDriveHub = async () => {
-  // BucketInfos, FileInfos
-};*/
-
-const deleteSDriveHubBackUp = async (infos) => {
-  const paths = [];
-  for (const info of infos) {
-    paths.push(info.path);
-  }
-
-  await deleteFiles(BACKUP_BUCKET, paths, 80);
-};
-
-/*const deleteBucketInfos = async () => {
-
-};*/
-
-const deleteFileInfos = async (infos) => {
-  const keys = [];
-  for (const info of infos) {
-    keys.push(datastore.key([FILE_INFO, info.path]));
-  }
-
-  await deleteData(keys, 200);
-};
-
-const getObsoleteFileLogs = async () => {
-  const dt = Date.now() - (31 * 24 * 60 * 60 * 1000);
-  const date = new Date(dt);
-
-  const query = datastore.createQuery(FILE_LOG);
-  query.filter(new PropertyFilter('createDate', '<', date));
-  query.order('createDate', { descending: false });
-  query.limit(20480);
-
-  const entities = await queryData(query, true);
-
-  const logs = [];
-  for (const entity of entities) {
-    const log = {
-      name: entity[datastore.KEY].name,
-      createDate: entity.createDate,
-    };
-    logs.push(log);
-  }
-  return logs;
-};
-
-const deleteFileLogs = async (logs) => {
-  const keys = [];
-  for (const log of logs) {
-    keys.push(datastore.key([FILE_LOG, log.name]));
-  }
-
-  await deleteData(keys, 1024);
-};
-
-const getObsoleteFileWorkLogs = async () => {
-  const dt = Date.now() - (31 * 24 * 60 * 60 * 1000);
-  const date = new Date(dt);
-
-  const query = datastore.createQuery(FILE_WORK_LOG);
-  query.filter(new PropertyFilter('createDate', '<', date));
-  query.order('createDate', { descending: false });
-  query.limit(752);
-
-  const entities = await queryData(query, true);
-
-  const logs = [];
-  for (const entity of entities) {
-    const log = {
-      id: entity[datastore.KEY].id,
-      createDate: entity.createDate,
-    };
-    logs.push(log);
-  }
-  return logs;
-};
-
-const deleteFileWorkLogs = async (logs) => {
-  const keys = [];
-  for (const log of logs) {
-    keys.push(datastore.key([FILE_WORK_LOG, datastore.int(log.id)]));
-  }
-
-  await deleteData(keys, 256);
-};
-
 const data = {
   getObsoleteVerifyLogs, deleteVerifyLogs, getObsoleteNotifyLogs, deleteNotifyLogs,
   getObsoleteAcknowledgeLogs, deleteAcknowledgeLogs, getObsoleteBraceStaticFiles,
   deleteBraceStaticFiles, getExtractEntities, getObsoleteExtracts, deleteExtracts,
-  getObsoleteExtractLogs, deleteExtractLogs, getDeletedFileInfos, deleteSDriveHubBackUp,
-  deleteFileInfos, getObsoleteFileLogs, deleteFileLogs, getObsoleteFileWorkLogs,
-  deleteFileWorkLogs,
+  getObsoleteExtractLogs, deleteExtractLogs,
 };
 
 export default data;
